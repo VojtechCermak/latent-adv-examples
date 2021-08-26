c7c970839618b14b0e36c483731c66d5a8c425c2
lukas.adam.cr@gmail.com <lukas.adam.cr@gmail.com>
2021-08-26 18:15:39 +0200

    Scripts updated

---
+++ experiments/run_experiments.sh
@@ -0,0 +1,4 @@
+python main.py -p "jsons/mnist_targeted.json" -o "results"
+python main.py -p "jsons/mnist_untargeted.json" -o "results"
+python main.py -p "jsons/svhn_targeted.json" -o "results"
+python main.py -p "jsons/svhn_untargeted.json" -o "results"
