f0ea0ae668117438949fc739ed1975aced374722
vojta-cermak <cermak.vojtech@seznam.cz>
2021-09-06 15:22:48 +0200

    cleanup

--- experiments/run_experiments.sh
+++
@@ -1,4 +0,0 @@
-python main.py -p "jsons/mnist_targeted.json" -o "results"
-python main.py -p "jsons/mnist_untargeted.json" -o "results"
-python main.py -p "jsons/svhn_targeted.json" -o "results"
-python main.py -p "jsons/svhn_untargeted.json" -o "results"
